ef7adbe7dace20b444e63e2a62fd4a77d1bf8285
ProbablyanAsian <43358667+probablyanasian@users.noreply.github.com>
2019-12-16 13:20:56 -0800

    Modded

--- main.py
+++ main.py
@@ -1,21 +1,56 @@
 #!/usr/bin/env python3
 import os
 import sys
+import secrets
 from subprocess import Popen, STDOUT, PIPE
 
 index = 0
+
+while True:
+    opt = input('Randomize play order? (Y/n): ').lower()
+    if opt in ['yes', 'y']:
+        randomize = True
+        break
+    elif opt in ['', ' ', '  ']:
+        continue 
+    else: 
+        randomize = False
+        break
+
+while True:
+    opt = input('Play without video? (Y/n): ').lower()
+    if opt in ['yes', 'y']:
+        novideo = True
+        break
+    elif opt in ['', ' ', '  ']:
+        continue
+    else:
+        novideo = False
+        break
+
 while True:
     try:
         songlist = open('songlist.txt', 'r')
         opensonglist = songlist.readlines()
         songs = [x.rstrip() for x in opensonglist]
-        p = Popen([f'cvlc {songs[index]} --play-and-exit'], shell=True)
+
+        if randomize:
+            index = secrets.randbelow(len(songs))
+
+        string = f'cvlc {songs[index]} --play-and-exit'
+
+        if novideo:
+            string += ' --novideo'
+
+        print(f'Playing index: {index+1}')
+        p = Popen([string], shell=True)
         songlist.close()
         p.wait()
 
-        if index == len(songs):
-            index = 0
-        else: index += 1
+        if not randomize:
+            if index == len(songs):
+                index = 0
+            else: index += 1
 
     except KeyboardInterrupt:
         sys.exit(0)
